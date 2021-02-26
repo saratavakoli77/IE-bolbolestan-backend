e820f5c3eb338dbae8f27a8b97ae01e6e565a647
Ghazal Minaei <ghminaei1037@gmail.com>
2021-02-26 22:29:20 +0330

    Feat: add top module

---
+++ src/main/java/bolbolestan/Bolbolestan.java
@@ -0,0 +1,11 @@
+package bolbolestan;
+
+
+import bolbolestan.userInterface.TerminalServer;
+
+public class Bolbolestan {
+    public static void main(String[] args) {
+        TerminalServer terminalServer = new TerminalServer();
+        terminalServer.runServer();
+    }
+}
