fef9b217e0a95a467118a26917afe048a0df9d9d
saratvk <saratvk1377@gmail.com>
2021-02-27 23:32:01 +0330

    Feat: add StudentStorage

---
+++ src/main/java/bolbolestan/student/StudentStorage.java
@@ -0,0 +1,27 @@
+package bolbolestan.student;
+
+import bolbolestan.bolbolestanExceptions.StudentNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentStorage {
+    private static final List<StudentEntity> studentEntities = new ArrayList<>();
+
+    public static void add(StudentEntity studentEntity) {
+        studentEntities.add(studentEntity);
+    }
+
+    public static void remove(StudentEntity studentEntity) {
+        studentEntities.remove(studentEntity);
+    }
+
+    public static StudentEntity getById(String studentId) throws StudentNotFoundException {
+        for (StudentEntity entity: studentEntities) {
+            if (entity.getStudentId().equals(studentId)) {
+                return entity;
+            }
+        }
+        throw new StudentNotFoundException();
+    }
+}
