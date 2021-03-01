1930b8725066eeddf4ba0112a6abf4f452a185c3
Ghazal Minaei <ghminaei1037@gmail.com>
2021-03-01 21:50:36 +0330

    Chore: replace infinite loop

--- src/main/java/bolbolestan/Bolbolestan.java
+++ src/main/java/bolbolestan/Bolbolestan.java
@@ -6,8 +6,6 @@ import bolbolestan.userInterface.TerminalServer;
 public class Bolbolestan {
     public static void main(String[] args) {
         TerminalServer terminalServer = new TerminalServer();
-        for (;;) {
-            terminalServer.listen();
-        }
+        terminalServer.listen();
     }
 }
