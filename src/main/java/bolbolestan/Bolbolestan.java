ae6452ed1f783a176296586f4b351e785862fed7
saratvk <saratvk1377@gmail.com>
2021-03-01 19:37:34 +0330

    Fix: Bolbolestan infinite loop

--- src/main/java/bolbolestan/Bolbolestan.java
+++ src/main/java/bolbolestan/Bolbolestan.java
@@ -6,6 +6,8 @@ import bolbolestan.userInterface.TerminalServer;
 public class Bolbolestan {
     public static void main(String[] args) {
         TerminalServer terminalServer = new TerminalServer();
-        terminalServer.listen();
+        for (;;) {
+            terminalServer.listen();
+        }
     }
 }
