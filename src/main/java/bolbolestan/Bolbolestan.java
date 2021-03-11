5ea4b73aa5f6cfdb58d2b4bc4d776ae79711d580
Ghazal Minaei <ghminaei1037@gmail.com>
2021-03-11 23:04:03 +0330

    Chore: migrate to tomcat

--- src/main/java/bolbolestan/Bolbolestan.java
+++ src/main/java/bolbolestan/Bolbolestan.java
@@ -4,14 +4,14 @@ package bolbolestan;
 import bolbolestan.offering.OfferingStorage;
 import bolbolestan.offeringRecord.OfferingRecordStorage;
 import bolbolestan.student.StudentStorage;
-import bolbolestan.userInterface.WebServer;
+//import bolbolestan.userInterface.WebServer;
 
 public class Bolbolestan {
-    private static WebServer server;
+//    private static WebServer server;
     public static void main(String[] args) throws Exception {
         fillStorages();
-        server = new WebServer();
-        server.start(8089);
+//        server = new WebServer();
+//        server.start(8089);
     }
 
     public static void fillStorages() {
