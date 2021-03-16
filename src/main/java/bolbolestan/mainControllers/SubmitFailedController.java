e93c915476ecd448d2a88c347d658c2a67ac2e1c
saratvk <saratvk1377@gmail.com>
2021-03-17 00:13:08 +0330

    Feat: add error page

---
+++ src/main/java/bolbolestan/mainControllers/SubmitFailedController.java
@@ -0,0 +1,24 @@
+package bolbolestan.mainControllers;
+
+import bolbolestan.middlewares.Authentication;
+import bolbolestan.student.StudentEntity;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/submit_failed/")
+public class SubmitFailedController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        StudentEntity authenticatedStudent = Authentication.getAuthenticated();
+        if (authenticatedStudent != null) {
+            request.getRequestDispatcher("/WEB-INF/submit_failed.jsp").forward(request, response);
+        } else {
+            response.sendRedirect(request.getContextPath() + "/login/");
+        }
+    }
+}
---
+++ src/main/webapp/WEB-INF/submit_failed.jsp
@@ -0,0 +1,39 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 98919
+  Date: 3/16/2021
+  Time: 11:36 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page isELIgnored="false" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Submit Failed</title>
+    <style>
+        h1 {
+            color: rgb(207, 3, 3);
+        }
+    </style>
+</head>
+<body>
+<a href="/">Home</a>
+<h1>
+    Error:
+
+</h1>
+<br>
+<h3>
+    <c:forEach var="error" items="${errorList}">
+        <tr>
+            <td><c:out value="${error}"/></td>
+        </tr>
+    </c:forEach>
+
+</h3>
+</body>
+</html>
