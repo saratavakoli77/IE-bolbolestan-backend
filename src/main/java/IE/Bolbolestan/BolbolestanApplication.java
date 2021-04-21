7d8c42ae6e479bf788aea786a25d65f2085950a5
Ghazal Minaei <ghminaei1037@gmail.com>
2021-04-22 00:41:30 +0430

    Feat: add checkListJob scheduler

--- src/main/java/IE/Bolbolestan/BolbolestanApplication.java
+++ src/main/java/IE/Bolbolestan/BolbolestanApplication.java
@@ -4,22 +4,23 @@ import IE.Bolbolestan.offering.OfferingEntity;
 import IE.Bolbolestan.offering.OfferingStorage;
 import IE.Bolbolestan.offeringRecord.OfferingRecordStorage;
 import IE.Bolbolestan.student.StudentStorage;
+import IE.Bolbolestan.waitList.CheckListJob;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class BolbolestanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BolbolestanApplication.class, args);
+		ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 		fillStorages();
-
-		for (OfferingEntity offeringEntity: OfferingStorage.getAllOfferings()) {
-			offeringEntity.setPrerequisites(new ArrayList<>());
-			offeringEntity.setCapacity(2);
-		}
+		scheduler.scheduleAtFixedRate(new CheckListJob(), 0, 15, TimeUnit.MINUTES);
 	}
 
 	public static void fillStorages() {
