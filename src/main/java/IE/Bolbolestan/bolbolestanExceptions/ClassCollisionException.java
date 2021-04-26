f10e0500503d85eb8e89827c3909802479d4248f
saratvk <saratvk1377@gmail.com>
2021-04-26 18:03:07 +0430

    fix: collision error messages changed

--- src/main/java/IE/Bolbolestan/bolbolestanExceptions/ClassCollisionException.java
+++ src/main/java/IE/Bolbolestan/bolbolestanExceptions/ClassCollisionException.java
@@ -2,6 +2,7 @@ package IE.Bolbolestan.bolbolestanExceptions;
 
 public class ClassCollisionException extends Exception {
     public ClassCollisionException(String classCode1, String classCode2) {
-        super("Class time code " + classCode1 + " and class time code " + classCode2 + " collide.");
+        super(" کلاس شماره " + classCode1 + " و کلاس شماره " + classCode2 + " با هم تداخل دارند. ");
+        //"Class time code " + classCode1 + " and class time code " + classCode2 + " collide."
     }
 }
--- src/main/java/IE/Bolbolestan/bolbolestanExceptions/ExamTimeCollisionException.java
+++ src/main/java/IE/Bolbolestan/bolbolestanExceptions/ExamTimeCollisionException.java
@@ -2,6 +2,7 @@ package IE.Bolbolestan.bolbolestanExceptions;
 
 public class ExamTimeCollisionException extends Exception {
     public ExamTimeCollisionException(String classCode1, String classCode2) {
-        super("Exam time of class code " + classCode1 + " and exam time of class code " + classCode2 + " collide.");
+        super(" امتحان کلاس شماره " + classCode1 + " و امتحان کلاس شماره " + classCode2 + " با هم تداخل دارند. ");
+        //super("Exam time of class code " + classCode1 + " and exam time of class code " + classCode2 + " collide.");
     }
 }
